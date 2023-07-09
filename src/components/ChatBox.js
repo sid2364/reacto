cf0ce6ac8d341e81e399e20f6632c3b7ca453812
Shreya <shreya.shenoy17@gmail.com>
2023-07-09 13:18:08 -0500

    adding workflow

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,63 +1,63 @@
 import React, { useEffect, useRef, useState } from "react";
 import {
-    query,
-    collection,
-    orderBy,
-    onSnapshot,
-    limit,
+  query,
+  collection,
+  orderBy,
+  onSnapshot,
+  limit,
 } from "firebase/firestore";
 import { auth, db } from "../firebase";
 import Message from "./Message";
 import SendMessage from "./SendMessage";
-import ClearMessages from "./ClearMessages"
+import ClearMessages from "./ClearMessages";
 import TypingDots from "./TypingDots";
 
 const ChatBox = () => {
-    const { displayName } = auth.currentUser;
-    const [messages, setMessages] = useState([]);
-    const [typing, setTyping] = useState(false)
+  const { displayName } = auth.currentUser;
+  const [messages, setMessages] = useState([]);
+  const [typing, setTyping] = useState(false);
 
-    const scroll = useRef();
+  const scroll = useRef();
 
-    useEffect(() => {
-        const q = query(
-            collection(db, "messages"),
-            orderBy("createdAt"),
-            limit(50)
-        );
-
-        const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
-            let messages = [];
-            QuerySnapshot.forEach((doc) => {
-                messages.push({ ...doc.data(), id: doc.id });
-            });
-            setMessages(messages);
-        });
-        return () => unsubscribe;
-    }, []);
-
-    function handleTypingMessage() {
-        scroll.current.scrollIntoView({ behavior: "smooth" });
-        setTyping(true)
-        console.log("typing...")
-        setTimeout(() => {
-            setTyping(false)
-        }, 2000);
-    }
-
-    return (
-        <main className="chat-box">
-            <div className="messages-wrapper">
-                {messages?.map((message) => (
-                    <Message key={message.id} message={message} />
-                ))}
-                { typing ? <TypingDots scroll={scroll} user={displayName}/> : null }
-            </div>
-            <span ref={scroll}></span>
-            <SendMessage scroll={scroll} handleTypingMessage={handleTypingMessage} />
-            <ClearMessages/>
-        </main>
+  useEffect(() => {
+    const q = query(
+      collection(db, "messages"),
+      orderBy("createdAt"),
+      limit(50),
     );
+
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      let messages = [];
+      QuerySnapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
+    });
+    return () => unsubscribe;
+  }, []);
+
+  function handleTypingMessage() {
+    scroll.current.scrollIntoView({ behavior: "smooth" });
+    setTyping(true);
+    console.log("typing...");
+    setTimeout(() => {
+      setTyping(false);
+    }, 2000);
+  }
+
+  return (
+    <main className="chat-box">
+      <div className="messages-wrapper">
+        {messages?.map((message) => (
+          <Message key={message.id} message={message} />
+        ))}
+        {typing ? <TypingDots scroll={scroll} user={displayName} /> : null}
+      </div>
+      <span ref={scroll}></span>
+      <SendMessage scroll={scroll} handleTypingMessage={handleTypingMessage} />
+      <ClearMessages />
+    </main>
+  );
 };
 
 export default ChatBox;
--- src/components/TypingDots.js
+++ src/components/TypingDots.js
@@ -1,18 +1,18 @@
 import React from "react";
 
 const TypingDots = ({ scroll, user }) => {
-    if (scroll) {
-        setTimeout(() => {
-            scroll.current.scrollIntoView({ behavior: "smooth" });
-        }, 100);
-    }
-    return (
-        <div className="typing-dots">
-            <div className="typing__center">
-                <p className="user-message">{user} is typing...</p>
-            </div>
-        </div>
-    );
+  if (scroll) {
+    setTimeout(() => {
+      scroll.current.scrollIntoView({ behavior: "smooth" });
+    }, 100);
+  }
+  return (
+    <div className="typing-dots">
+      <div className="typing__center">
+        <p className="user-message">{user} is typing...</p>
+      </div>
+    </div>
+  );
 };
 
 export default TypingDots;
